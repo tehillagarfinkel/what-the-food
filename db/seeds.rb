816b8d8ebf6718da70e8b390a52028d53c86b8cb
Tehilla Garfinkel <tehillagarfinkel613@gmail.com>
2019-10-03 18:19:34 -0500

    adds more questions to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Question.create!(query: "Would you rather eat the same thing every day or never be able to eat the same thing twice?")
+Question.create!(query: "If it meant you could save pizza from extinction, would you eat a cricket?")
+Question.create!(query: "Willing to walk or was today leg day?")
+Question.create!(query: "If you were responsible for choosing whether the world would have bagels or donuts, which one would you choose?")
+Question.create!(query: "How spicy do you like your food?")
+Question.create!(query: "Favorite reality TV show?")
+Question.create!(query: "Would you rather eat potatoes with every meal or rice with every meal?")
+Question.create!(query: "Of the following, what is your favorite fast food restaurant?")
+Question.create!(query: "Do you have a sweet tooth?")
+vQuestion.create!(query: "What outfit are you wearing right now?")
+Question.create!(query: "How adventurous are you feeling?")
+Question.create!(query: "Which mythological creature do you wish were real?")
+Question.create!(query: "Do you grow your own herbs?")
+Question.create!(query: "What unusual food would you prefer?")
+Question.create!(query: "Favorite breakfast cereal:")
+Question.create!(query: "How high are you right now?")
+Question.create!(query: "Which type of movie would you rather see?")
+Question.create!(query: "Is bowling a sport?")
+Question.create!(query: "Pick a sport")
+Question.create!(query: "Pick an amusement park ride")
+Question.create!(query: "Which best describes your current mood?")
+Question.create!(query: "What's your favorite vegetable?")
+Question.create!(query: "Last minute reservation?")
+Question.create!(query: "How many slices of pizza can you eat in a single sitting?")
