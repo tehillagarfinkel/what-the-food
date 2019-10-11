7867ad62b129626b43a9c6ea7176d44bdefce8a3
Adam Keune <akeune23@gmail.com>
2019-10-10 19:10:20 -0500

    Add api namesapce, results controller and index action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,15 @@
 class PagesController < ApplicationController
   def index
-    response = HTTP
-      .headers({
-        "X-User-Email" => ENV["YELP_API_EMAIL"],
-        "Authorization" => "Bearer #{ENV["YELP_API_KEY"]}",
-      })
-      .get("https://api.yelp.com/v3/businesses/search", params: {
-                                                          location: 60601,
-                                                          radius: 4000,
-                                                        })
-      .parse
+    # response = HTTP
+    #   .headers({
+    #     "X-User-Email" => ENV["YELP_API_EMAIL"],
+    #     "Authorization" => "Bearer #{ENV["YELP_API_KEY"]}",
+    #   })
+    #   .get("https://api.yelp.com/v3/businesses/search", params: { location: 60601, radius: 4000, open_now: true, categories: "Restaurants" })
+    #   .parse
 
-    render json: response
+    # render json: response
   end
 end
+
+# params["zip_code"] / params["radius"]
