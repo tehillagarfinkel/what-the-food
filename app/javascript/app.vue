7e5a0004c82c3d46e6a6cbe783d02b6142b9c6d4
heydanhey <danhey@gmail.com>
2019-10-28 09:05:44 -0500

    Added Vue Router

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
+    <router-link to="/">Home</router-link>
+    <router-link to="/login">Login</router-link>
+    <router-link to="/signup">Signup</router-link>
     <p>{{ message }}</p>
+    <router-view></router-view>
   </div>
 </template>
 
