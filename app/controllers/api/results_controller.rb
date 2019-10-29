b456df6b260e29643af7c34098415080ffa761cf
Adam Keune <akeune23@gmail.com>
2019-10-28 20:53:46 -0500

    Add Home and Question router-views; outline M/C and slider question formats

--- app/controllers/api/results_controller.rb
+++ app/controllers/api/results_controller.rb
@@ -1,6 +1,6 @@
 class Api::ResultsController < ApplicationController
   def index
-    response = HTTP
+    @restaurants = HTTP
       .headers({
         "X-User-Email" => ENV["YELP_API_EMAIL"],
         "Authorization" => "Bearer #{ENV["YELP_API_KEY"]}",
@@ -8,6 +8,7 @@ class Api::ResultsController < ApplicationController
       .get("https://api.yelp.com/v3/businesses/search", params: { location: 60601, radius: 4000, open_now: true, categories: "Restaurants" })
       .parse
 
-    render json: response
+    render "index.json.jb"
+    # render json: response
   end
 end
--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -12,7 +12,7 @@
 export default {
   data: function() {
     return {
-      message: "Hello Vue!"
+      message: "What The Food?!"
     };
   }
 };
