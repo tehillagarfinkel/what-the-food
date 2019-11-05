fd6419689a35321327d9497a796246c34e817b30
kaylasilvey <ksilvey19@gmail.com>
2019-11-04 21:02:16 -0600

    formats data for search filter

--- app/javascript/search_filter.js
+++ app/javascript/search_filter.js
@@ -196,3 +196,236 @@
   name: "Italian",
   answer_ids: []
 },
+
+{
+name: "Japanese",
+answer_ids: [],
+}
+{
+name: "Japanese Curry",
+answer_ids: [],
+}
+{
+name: "Western Style Japanese Food",
+answer_ids: [],
+}
+{
+name: "Jewish",
+answer_ids: [],
+}
+{
+name: "Korean",
+answer_ids: [],
+}
+{
+name: "Latin American",
+answer_ids: [],
+}
+{
+name: "Colombian",
+answer_ids: [],
+}
+{
+name: "Salvadoran",
+answer_ids: [],
+}
+{
+name: "Venezuelan",
+answer_ids: [],
+} 
+{
+name: "Malaysian",
+answer_ids: [],
+} 
+{
+name: "Mediterranean",
+answer_ids: [],
+} 
+{
+name: "Mexican",
+answer_ids: [],
+}
+{
+name: "Middle Eastern",
+answer_ids: [],
+}
+{
+name: "Egyptian",
+answer_ids: [],
+}
+{
+name: "Lebanese",
+answer_ids: [],
+}
+{
+name: "Modern Australian",
+answer_ids: [],
+}
+{
+name: "Modern European",
+answer_ids: [],
+}
+{
+name: "Mongolian",
+answer_ids: [],
+}
+{
+name: "Moroccan",
+answer_ids: [],
+}
+{
+name: "New Mexican Cuisine",
+answer_ids: [],
+}
+{
+name: "New Zealand",
+answer_ids: [],
+}
+{
+name: "Nicaraguan",
+answer_ids: [],
+}
+{
+name: "Oriental",
+answer_ids: [],
+}
+{
+name: "Pakistani",
+answer_ids: [],
+}
+{
+name: "Pan Asian",
+answer_ids: [],
+}
+{
+name: "Parma",
+answer_ids: [],
+}
+{
+name: "Persian/Iranian",
+answer_ids: [],
+}
+{
+name: "Peruvian",
+answer_ids: [],
+}
+{
+name: "Polynesian",
+answer_ids: [],
+}
+{
+name: "Portuguese",
+answer_ids: [],
+}
+{
+name: "Romanian",
+answer_ids: [],
+}
+{
+name: "Russian",
+answer_ids: [],
+}
+{
+name: "Scandinavian",
+answer_ids: [],
+}
+{
+name: "Scottish",
+answer_ids: [],
+}
+{
+name: "Seafood",
+answer_ids: [],
+}
+{
+name: "Singaporean",
+answer_ids: [],
+}
+{
+name: "Slovakian",
+answer_ids: [],
+}
+{
+name: "Somali",
+answer_ids: [],
+}
+{
+name: "Southern",
+answer_ids: [],
+}
+{
+name: "Spanish",
+answer_ids: [],
+}
+{
+name: "Sri Lankan",
+answer_ids: [],
+}
+{
+name: "Swedish",
+answer_ids: [],
+}
+{
+name: "Swiss Food",
+answer_ids: [],
+}
+{
+name: "Syrian",
+answer_ids: [],
+}
+{
+name: "Taiwanese",
+answer_ids: [],
+}
+{
+name: "Tex-Mex",
+answer_ids: [],
+}
+{
+name: "Thai",
+answer_ids: [],
+}
+{
+name: "Traditional Norwegian",
+answer_ids: [],
+}
+{
+name: "Traditional Swedish",
+answer_ids: [],
+}
+{
+name: "Turkish",
+answer_ids: [],
+}
+{
+name: "Ukrainian",
+answer_ids: [],
+}
+{
+name: "Uzbek",
+answer_ids: [],
+}
+{
+name: "Vietnamese",
+answer_ids: [],
+}
+{
+name: "Gluten-Free",
+answer_ids: [],
+}
+{
+name: "Halal",
+answer_ids: [],
+}
+{
+name: "Kosher",
+answer_ids: [],
+}
+{
+name: "Vegan",
+answer_ids: [],
+}
+{
+name: "Vegetarian",
+answer_ids: [],
+}
