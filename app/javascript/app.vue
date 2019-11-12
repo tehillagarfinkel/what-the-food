3a96f6b11e37255a3ceb5bfbd6f78b58f9036361
kaylasilvey <ksilvey19@gmail.com>
2019-11-11 19:21:06 -0600

    reformats page layout, sets background image

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,14 +1,43 @@
 <template>
-  <div id="app">
-    <nav class="nav">
-      <router-link to="/" class="nav-link">Home</router-link>
-      <router-link to="/login" class="nav-link">Login</router-link>
-      <router-link to="/signup" class="nav-link">Signup</router-link>
+  <div id="app" class="bg">
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light static-top mb-5 shadow opaque">
+      <div class="container">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <router-link to="/" class="nav-link">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/login" class="nav-link">Login</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/signup" class="nav-link">Signup</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
 
-    <body class="bg">
-      <router-view></router-view>
-    </body>
+    <!-- Page Content -->
+    <div class="container opaque">
+      <div class="card border-0 shadow my-5">
+        <div class="card-body p-5">
+          <router-view></router-view>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -27,16 +56,17 @@ p {
   font-size: 2em;
   text-align: center;
 }
-
-body,
-html {
-  height: 100%;
-}
 .bg {
-  background-image: url("https://images.unsplash.com/photo-1550949987-33f716ccc232?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2808&q=80");
-  height: 100%;
-  background-position: center;
-  background-repeat: no-repeat;
+  background: url("https://images.unsplash.com/photo-1550949987-33f716ccc232?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2808&q=80")
+    no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
   background-size: cover;
+  -o-background-size: cover;
+  min-height: 900px;
+}
+
+.opaque {
+  opacity: 0.9;
 }
 </style>
